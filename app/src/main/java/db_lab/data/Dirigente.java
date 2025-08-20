33edd5e3a4b26f7c95a740554c54a8b4f7299bb5
simoncino <simone.capacci3@studio.unibo.it>
2025-08-20 23:05:14 +0200

    client

--- app/src/main/java/db_lab/data/Dirigente.java
+++ app/src/main/java/db_lab/data/Dirigente.java
@@ -1,5 +1,14 @@
 package db_lab.data;
 
 public class Dirigente {
-    
+public final String CF;
+ public final String nome;
+public final String cognome;
+public final String ruolo;
+    public Dirigente(String CF, String nome, String cognome, String ruolo ){
+         this.cognome=cognome;
+        this.nome=nome;
+        this.CF=CF;
+        this.ruolo=ruolo;
+    }
 }
