c8a50289693c89306352c89b6aab5d6d25883f73
simoncino <simone.capacci3@studio.unibo.it>
2025-08-20 22:48:50 +0200

    nuoveeh

--- app/src/main/java/db_lab/data/Calciatore.java
+++ app/src/main/java/db_lab/data/Calciatore.java
@@ -1,14 +1,17 @@
 package db_lab.data;
 
-public class Calciatorecedutoinprestito {
+public class Calciatore {
+   
+    public final int numeroMaglia;
     public final int posizione;
     public final String nome;
     public final String cognome;
     public final String CF;
 
-    public Calciatorecedutoinprestito(int posizione, String nome, String cognome, String CF ){
+    public Calciatore(int posizione, int numeroMaglia, String nome, String cognome, String CF ){
         this.posizione=posizione;
         this.cognome=cognome;
+        this.numeroMaglia=numeroMaglia;
         this.nome=nome;
         this.CF=CF;
     }
--- app/src/main/java/db_lab/data/Calciatorevenduto.java
+++ app/src/main/java/db_lab/data/Calciatorevenduto.java
@@ -1,5 +1,15 @@
 package db_lab.data;
 
 public class Calciatorevenduto {
-    
+      public final int posizione;
+    public final String nome;
+    public final String cognome;
+    public final String CF;
+
+    public Calciatorevenduto(int posizione, String nome, String cognome, String CF ){
+        this.posizione=posizione;
+        this.cognome=cognome;
+        this.nome=nome;
+        this.CF=CF;
+    }
 }
