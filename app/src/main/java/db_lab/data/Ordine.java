4f4d97f803f07ab1d33ce6c0574fb025f4c99b20
simoncino <simone.capacci3@studio.unibo.it>
2025-08-20 23:13:15 +0200

    fatto

---
+++ app/src/main/java/db_lab/data/Ordine.java
@@ -0,0 +1,15 @@
+package db_lab.data;
+
+public class Ordine {
+
+    public final int codiceOrdine;
+    public final int data;
+    public final int rimborsato;
+
+      public Ordine(int codiceOrdine, int data, int rimborsato){
+        this.codiceOrdine=codiceOrdine;
+        this.data=data;
+        this.rimborsato=rimborsato;
+      }
+    
+}
