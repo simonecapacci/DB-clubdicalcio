bf565f4f43ab83ab5d8c186a32189c7674056cc1
simoncino <simone.capacci3@studio.unibo.it>
2025-08-20 22:28:34 +0200

    nuovee

--- app/src/main/java/db_lab/data/Articolo_generale.java
+++ app/src/main/java/db_lab/data/Articolo_generale.java
@@ -2,8 +2,11 @@ package db_lab.data;
 
 public class Articolo_generale {
     public final String tipoArticolo;
-
-    public Articolo_generale(String tipoArticolo){
+    public final int codiceProdotto;
+    public final int importo;
+    public Articolo_generale(String tipoArticolo, int codiceProdotto, int importo){
         this.tipoArticolo=tipoArticolo;
+         this.importo=importo;
+        this.codiceProdotto=codiceProdotto;
     }
 }
