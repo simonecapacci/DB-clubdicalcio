87721773cd8d3a7c0385842e9eaa14f0a43e2b20
Enrico Casadei <enrico.casadei18@studio.unibo.it>
2025-08-23 16:28:13 +0200

    fix di Calciatore.java

--- app/src/main/java/db_lab/data/Calciatore.java
+++ app/src/main/java/db_lab/data/Calciatore.java
@@ -1,5 +1,10 @@
 package db_lab.data;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calciatore {
    
     public final int numeroMaglia;
@@ -14,5 +19,46 @@ public class Calciatore {
         this.numeroMaglia=numeroMaglia;
         this.nome=nome;
         this.CF=CF;
+    }
+    public static final class DAO{
+
+        public static List<AssistPersonali> getTopAssistmen(Connection connection) {
+            List<AssistPersonali> list = new ArrayList<>();
+            try (
+                var preparedStatement = DAOUtils.prepare(connection, Queries.BEST_PLAYERS_BY_ASSISTS);
+                var resultList = preparedStatement.executeQuery();
+            ) {
+                while (resultList.next()){
+                    var nome = resultList.getString("Nome");
+                    var cognome = resultList.getString("Cognome");
+                    var assists = resultList.getInt("SUM(ASSIST)");
+                    list.add(new AssistPersonali(nome,cognome,assists));
+                }
+                
+            } catch (SQLException e) {
+                throw new DAOException(e);
+            }
+            return list;
+        }
+
+        public static List<GoalPersonali> getTopScorers(Connection connection) {
+            List<GoalPersonali> list = new ArrayList<>();
+            try (
+                var preparedStatement = DAOUtils.prepare(connection, Queries.BEST_PLAYERS_BY_GOALS);
+                var resultList = preparedStatement.executeQuery();
+            ) {
+                while (resultList.next()){
+                    var nome = resultList.getString("Nome");
+                    var cognome = resultList.getString("Cognome");
+                    var assists = resultList.getInt("SUM(GOL)");
+                    list.add(new GoalPersonali(nome,cognome,assists));
+                }
+                
+            } catch (SQLException e) {
+                throw new DAOException(e);
+            }
+            return list;
+        }
+
     }
 }
